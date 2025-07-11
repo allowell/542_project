9b54399e04376b9b60e220d0014953e153e2d1cb
Allison McDowell <allisonmcdowell02@gmail.com>
2025-07-11 11:35:54 -0400

    work with lme models

---
+++ Allison Code.R
@@ -0,0 +1,32 @@
+
+#Mixed effects model
+#library(lme4)
+#library(Matrix)
+library(nlme)
+#Deciding to treat estabp as a binary for right now to look at comparison
+#between sites with no carryover versus any carryover
+
+#Data in factors for mixed effects model
+df <- hyp1 |>
+  mutate(
+    stdy = factor(stdy),
+    plot = factor(plot),
+    TRT_CODE = factor(TRT_CODE),
+    yst = factor(yst),
+    estabp_bin = factor(ifelse(estabp > 0, 1, 0)),
+    totp_sc = scale(totp)
+  )
+
+
+#First model
+#Response: height, random intercepts for site and plot
+m_mod1 <- lmer(ht ~ totp_sc * estabp_bin + (1 | stdy/plot), data = df)
+
+#Second model
+hyp1_complete <- na.omit(hyp1[, c("ht", "yst", "cump", "estabp", "stdy", "plot")])
+model <- lme(ht ~ yst + cump + estabp, 
+             random = ~ 1 | stdy, data = hyp1_complete)
+summary(model)
+ranef(model)
+
+#See some significant intercept differences between studies
--- allison_code_initial.R
+++ allison_code_initial.R
@@ -1,5 +1,5 @@
 
-Loading in the data
+#Loading in the data
 data <- read.csv("RW18_28_stdyPLOTlong.csv", header = T)
 
 #install.packages("readxl")
@@ -10,25 +10,18 @@ treegrowth <- read_excel("RW28 data_foliar_and_soil_nutrients.xlsx", sheet = "tr
 treatments <- read_excel("RW28 data_foliar_and_soil_nutrients.xlsx", sheet = "treatments")
 plotinfo <- read_excel("RW28 data_foliar_and_soil_nutrients.xlsx", sheet = "plotinfo")
 
-maindata <- treegrowth[,c(1,4,5,7)]
+plotinfo_main <- plotinfo[, c(1:6, 8)]
+treatments_main <- treatments[, c(2:7)]
 
 
 #Merging
 library(dplyr)
 library(stringr)
 
-plotinfo_subset <- plotinfo %>%
-  select(STDY, PLOT, TRT_CODE, TRT_VARIATION, SIZE_TREAT)
+treatments_main <- treatments_main %>%
+  mutate(TRT_CODE = str_pad(as.character(TRT_CODE), width = 3, pad = "0"))
 
-first_combined <- maindata %>%
-  left_join(plotinfo_subset, by = c("STDY", "PLOT"))
-
-treatments <- treatments %>%
-  mutate(TRT_CODE = str_pad(as.character(TRT_CODE), width = 3, pad = "0")) %>%
-  select(TRT_CODE, TRT_VARIATION, TRT_DESCRIP, N_RATE, P_RATE, K_RATE)
-
-main_combine <- first_combined %>%
-  left_join(treatments, by = c("TRT_CODE", "TRT_VARIATION"))
+main_plotinfo <- left_join(plotinfo_main, treatments_main, by = c("TRT_CODE", "TRT_VARIATION"))
 
 
-#Yay, looks like main_combine has the proper data now!
+#Yay, looks like main_plotinfo has the proper data now!
